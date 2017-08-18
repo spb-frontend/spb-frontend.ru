98853de72588b127b3bec397ec723ce458d19d85
Alexander Kurganov <me@akurganow.ru>
2017-08-18 13:37:12 +0300

    Updates

---
+++ path---podcast-7604eb5602b8891422a1.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xe0c58a9120b25000],{"./node_modules/json-loader/index.js!./.cache/json/podcast.json":function(e,t){e.exports={data:{allContentfulDrinkcast:{edges:[{node:{title:"#1 — «12 колонок на пакет мертвеца»",date:"2017-07-16T00:00+03:00"}},{node:{title:"#2 — «Бойкий Джун — Счастливый Ментор»",date:"2017-08-04"}},{node:{title:"#3 — «Раньше дивы были зеленее»",date:"2017-08-11"}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---podcast-7604eb5602b8891422a1.js.map
